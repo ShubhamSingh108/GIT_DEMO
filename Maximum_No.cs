90ca327f9087a15c0e62d548800b7bdac2326b7f
“Shubham <shubham.singh@razorleaf.com>
2023-04-11 15:08:11 +0530

    changes committed

--- Maximum_No.cs
+++ Maximum_No.cs
@@ -2,7 +2,10 @@ using System;
 using System.Linq;
 class Demo {
    static void Main() {
+	   
+	   //array of Integer Number...
       int[] arr = { 20, 50, -35, 25, 60 };
+	  //finding  Maximum Number in Array
       Console.WriteLine(arr.Max());
    }
 }
